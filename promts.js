36b2c51176606e00c8d74366e21a7fc738174c7c
Roger Campbell II <mr@rogercampbell.io>
2021-09-28 14:14:02 -0400

    Prushing up prompts

---
+++ promts.js
@@ -0,0 +1,54 @@
+// Data Types
+// Prompt 1: Create one variable with var, const, and let. Set there values to any data type you would like.
+
+// Prompt 2: Create two constant variables that store a array of strings and numbers respectively
+
+// Prompt 3: Create an object that store at least 3 key value pairs. The data types that each store is up to you.
+
+//Prompt 4: Print to the console each value stored within the object
+
+// Prompt 5: Change each value stored within the object to any new value of your choice
+
+// Prompt 6: Print to the console each value stored within the object
+
+//Section: Number Arrays
+
+// Prompt 1: Write a for loop that iterates over the numbers array and prints each value to the console
+
+// Prompt 2: Utilize the forEach method to iterate over the numbers array and prints each value to the console
+
+// Prompt 3: Create an variable called forLoopSquaredNumbersArray. Write a for loop that iterates over the numbers array and squares each number and then adds the squared number to the array stored within the forLoopSquaredNumbersArray variable. Print the new array to the console.
+
+// Prompt 4: Utilize the map method to iterate over the numbers array and return each number squared. Print the new array to the console.
+
+// Prompt 5: Create an variable called isItLessThanForLoop. Write a for loop that iterates over the numbers array and checks to see if the number is less than 5. If it is, add that number to the array stored within the isItLessThanForLoop variable. Print the new array to the console.
+
+// Prompt 6: Utilize the filter method to iterate over the numbers array and return all numbers that are less than 5. Print the new array to the console.
+
+// Section: String Arrays
+
+// Prompt 1: Create an variable called capitalizedNamesForLoopArray. Write a for loop that iterates over the names array. For each name in the array capitalize every letter and add it to the array stored within the capitalizedNamesForLoopArray variable. Print the new array to the console.
+
+// Prompt 2: Utilize the map method to iterate over the numbers array and return each name fully capitalized. Then use the foreach method to iterate over the newly created array and print the each fully capitalized name to the console.
+
+// Prompt 3: Create an variable called names WithLengthLessThanOrEqualToFiveForLoopArray that stores an empty array. Write a for loop that iterates over the names array. For each name in the array check to see if the length of the name is less that or equal to 5. If so store the name in the namesWithLengthLessThanOrEqualToFiveForLoopArray array variable. Print the new array to the console.
+
+// Prompt 4: Utilize the filter method to iterate over the numbers array and return each name with the length of less than or equal to 5. Then use the foreach method to iterate over the newly created array and print each name to the console.
+
+// Section: Objects
+
+// Prompt 1: Create a random object and write two separate print statements:
+// 1. Which prints its keys
+// 2. Which prints its values
+
+// Section: Functions & Methods
+
+// Prompt 1: Create 4 functions utilizing the function declaration syntax:
+// 1. That adds two numbers
+// 2. That subtracts two numbers
+// 3. That multiples two numbers
+// 4. That divides two numbers
+
+// Prompt 2: Convert each of the above function declarations in to arrow functions
+
+// Prompt 3: Create a object called mathEquations. Inside the object convert each of the above arrow functions into a a method. Then invoke each method store its output in a variable and print each to the console.
